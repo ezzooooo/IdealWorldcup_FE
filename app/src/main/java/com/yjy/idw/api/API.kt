e2607a3d1aaf5ecb5196119e518a6da5775ca4db
leeyunbo <servers1@naver.com>
2020-06-08 01:45:52 +0900

    [개발] USER 데이터 관련 API 추가

---
+++ app/src/main/java/com/yjy/idw/api/API.kt
@@ -0,0 +1,21 @@
+package com.yjy.idw.api
+
+import com.yjy.idw.data.UserVO
+import retrofit2.Call
+import retrofit2.http.*
+
+interface API {
+
+    /* GET USER INFORMATION */
+    @GET("users")
+    fun getUserList() : Call<List<UserVO>>
+
+    @GET("users/{id}")
+    fun getUser(@Path("id")id: Int) : Call<UserVO>
+
+    @POST("/users")
+    fun insertUser(@Body vo: UserVO)
+
+    @DELETE("/users/{id}")
+    fun deleteUser(@Path("id")id: Int)
+}
