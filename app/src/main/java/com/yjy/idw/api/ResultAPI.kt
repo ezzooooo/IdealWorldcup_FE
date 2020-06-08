9dfe92f02a439b03c79bf85cde0109b56b70f6d8
leeyunbo <servers1@naver.com>
2020-06-09 00:53:59 +0900

    [개발] ResultAPI 개발 완료

--- app/src/main/java/com/yjy/idw/api/ResultAPI.kt
+++ app/src/main/java/com/yjy/idw/api/ResultAPI.kt
@@ -1,12 +1,19 @@
 package com.yjy.idw.api
 
 import com.yjy.idw.data.ResultVO
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.Call
+import retrofit2.http.*
 
 interface ResultAPI {
     @Headers("Accept:application/json")
     @POST("results")
     fun insertResult(@Body vo: ResultVO)
+
+    @Headers("Accept:application/json")
+    @PUT("results")
+    fun updateResult(@Body vo: ResultVO)
+
+    @Headers("Accept:application/json")
+    @GET("results")
+    fun getResult(@Query("first_image_id") first_image_id: Int, @Query("second_image_id") second_image_id: Int) : Call<ResultVO>
 }
--- app/src/main/java/com/yjy/idw/data/TournamentVO.kt
+++ app/src/main/java/com/yjy/idw/data/TournamentVO.kt
@@ -1,14 +1,26 @@
 package com.yjy.idw.data
 
+import com.google.gson.annotations.SerializedName
+
 data class TournamentVO(
+    @SerializedName("id")
     private var id : Int,
+    @SerializedName("user_id")
     private var user_id : Int,
+    @SerializedName("title")
     private var title : String,
+    @SerializedName("intro")
     private var intro : String,
+    @SerializedName("category")
     private var category : String,
+    @SerializedName("isPrivate")
     private var isPrivate : Int,
+    @SerializedName("passwd")
     private var passwd : String,
+    @SerializedName("like_cnt")
     private var like_cnt : Int,
+    @SerializedName("play_cnt")
     private var play_cnt : Int,
+    @SerializedName("sortBy")
     private var sortBy : String
 )
