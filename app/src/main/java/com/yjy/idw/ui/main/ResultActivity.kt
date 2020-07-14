206892c3f60ed2a2d83114989f019822f94473ad
leeyunbo <servers1@naver.com>
2020-07-14 16:19:15 +0900

    [개발] Result 액티비티 레이아웃 완료

---
+++ app/src/main/java/com/yjy/idw/ui/main/ResultActivity.kt
@@ -0,0 +1,24 @@
+package com.yjy.idw.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yjy.idw.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+
+    fun saveWinnerImage() {
+
+    }
+
+    fun startRankingActivity() {
+
+    }
+
+    fun shareWinnerImage() {
+        
+    }
+}
